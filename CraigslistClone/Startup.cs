36d327debadc3e60f68e253cdcc85faa2747ce6a
austincrisp <austin.crisp05@gmail.com>
2017-03-02 14:48:31 -0500

    Add project files.

---
+++ CraigslistClone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CraigslistClone.Startup))]
+namespace CraigslistClone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
